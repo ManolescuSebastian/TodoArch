fbf877698994e4bd08c28b06d8021006dd986f1e
Mihaly Nagy <mihaly-ferencz.nagy@ing.com>
2025-09-11 20:17:20 +0300

    Command center implementation

--- app/src/main/java/com/cd/todoarch/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/cd/todoarch/viewmodel/TaskViewModel.kt
@@ -20,30 +20,30 @@ import kotlinx.coroutines.launch
 
 class TaskViewModel : ViewModel() {
 
-    private val todoCommandCenter by inject<TodoFlux>()
+    private val todoFlux by inject<TodoFlux>()
 
-    val tasks: Flow<List<Task>> = todoCommandCenter.state.map { it.tasks }
+    val tasks: Flow<List<Task>> = todoFlux.state.map { it.tasks }
 
-    val loading: Flow<Boolean> = todoCommandCenter.state.map { it.loadingTasks }.distinctUntilChanged()
-    val tasksChangedEvents: Flow<TodoEvent> = todoCommandCenter.events.filter {
+    val loading: Flow<Boolean> = todoFlux.state.map { it.loadingTasks }.distinctUntilChanged()
+    val tasksChangedEvents: Flow<TodoEvent> = todoFlux.events.filter {
         it is TaskRemoved || it is TaskUpdated
     }
 
     fun addTask(title: String, description: String) {
-        executeCommand(AddTask(title, description))
+        execute(AddTask(title, description))
     }
 
     fun removeTask(task: Task) {
-        executeCommand(RemoveTask(task))
+        execute(RemoveTask(task))
     }
 
     fun updateTask(taskId: Int, title: String, description: String) {
-        executeCommand(UpdateTask(taskId, title, description))
+        execute(UpdateTask(taskId, title, description))
     }
 
-    private fun executeCommand(command: TodoKommand) {
+    private fun execute(command: TodoKommand) {
         viewModelScope.launch {
-            todoCommandCenter.execute(command)
+            todoFlux.execute(command)
         }
     }
 }
